137737b6d65b149e9f5ef23693cf8fc77c63e9ec
vaqz <64892623+vaqz@users.noreply.github.com>
2026-09-25 18:59:03 +0800

    Update cors.ts

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,6 +1,19 @@
 import { cors } from "hono/cors";
 
-// CORS Middleware
-const corsMiddleware = cors();
+const corsMiddleware = cors({
+	origin: "https://mail.vaqzmobiz.com",
+	allowMethods: [
+		"GET",
+		"POST",
+		"DELETE",
+		"PATCH",
+		"OPTIONS",
+	],
+	allowHeaders: [
+		"Content-Type",
+		"Authorization",
+	],
+	credentials: true,
+});
 
 export default corsMiddleware;
